92905d2074d32366a84dc8584348cc6cea5072a0
Roshan Patil <114917682+roshanpatil37407@users.noreply.github.com>
2024-08-08 19:41:59 +0530

    cc

---
+++ eventpayment.js
@@ -0,0 +1,17 @@
+document.getElementById("payBtn").addEventListener("click", function () {
+  document.getElementById("paymentModal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("paymentModal").style.display = "none";
+});
+
+document.getElementById("backBtn").addEventListener("click", function () {
+  document.getElementById("paymentModal").style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == document.getElementById("paymentModal")) {
+    document.getElementById("paymentModal").style.display = "none";
+  }
+});
