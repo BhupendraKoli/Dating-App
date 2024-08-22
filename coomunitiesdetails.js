d89fb46d756c3557b505c0984a7436233ca9d7b7
Roshan Patil <114917682+roshanpatil37407@users.noreply.github.com>
2024-08-22 22:28:15 +0530

    rp

---
+++ coomunitiesdetails.js
@@ -0,0 +1,37 @@
+// Get the modal
+var modal = document.getElementById("logoutModal");
+
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("logoutBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the confirm button
+var confirmBtn = document.getElementById("confirmLogout");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks on the confirm button, log out
+confirmBtn.onclick = function() {
+    alert("Logging out...");
+     window.location.href = 'HomePage.html';
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
